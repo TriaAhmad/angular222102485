eee75b116e0ea0f855108d7640ab7fa61744bae4
TriaAhmad <matchakubawa@gmail.com>
2024-10-25 10:05:03 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { RouterModule,Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { AdminComponent } from './admin/admin.component';
+import { RegisterComponent } from './register/register.component';
 
 
 export const routes: Routes = [
   {path: "",redirectTo: "login",pathMatch: "full"},
   {path: "admin",component: AdminComponent},
+  {path: "register",component: RegisterComponent},
   {path: "login",component: LoginComponent}
 ];
 
